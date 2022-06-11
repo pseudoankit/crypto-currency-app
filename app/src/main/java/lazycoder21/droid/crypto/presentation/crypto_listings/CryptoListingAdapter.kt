17b88f49c1057f068a52a9e41a439ccefe86ac52
Ankit <ankit.kumar1@tokopedia.com>
2022-06-11 18:23:18 +0530

    item view added

--- app/src/main/java/lazycoder21/droid/crypto/presentation/crypto_listings/CryptoListingAdapter.kt
+++ app/src/main/java/lazycoder21/droid/crypto/presentation/crypto_listings/CryptoListingAdapter.kt
@@ -1,21 +1,49 @@
 package lazycoder21.droid.crypto.presentation.crypto_listings
 
+import android.content.Context
+import android.text.Spannable
+import android.text.SpannedString
+import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.text.buildSpannedString
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import lazycoder21.droid.crypto.R
 import lazycoder21.droid.crypto.databinding.RvItemCryptoListingItemBinding
 import lazycoder21.droid.crypto.domain.model.CryptoDetail
+import lazycoder21.droid.crypto.utils.Utils.mColor
+import java.util.*
 
 class CryptoListingAdapter :
     ListAdapter<CryptoDetail, CryptoListingAdapter.ViewHolder>(DiffCallback()) {
 
-    class ViewHolder(val binding: RvItemCryptoListingItemBinding) :
+    class ViewHolder(private val binding: RvItemCryptoListingItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(item: CryptoDetail) {
+        fun bind(item: CryptoDetail) = with(binding) {
+            val context = binding.root.context
+            tvSymbolConv.text = buildSymbolConversionText(item, context)
+            tvPrice.text = item.lastPrice
+            tvPriceChange.text = calculatePriceChange(item)
+        }
 
+        private fun calculatePriceChange(item: CryptoDetail): String {
+            return ""
+        }
+
+        private fun buildSymbolConversionText(item: CryptoDetail, context: Context): SpannedString {
+            return buildSpannedString {
+                append(item.baseAsset?.uppercase(Locale.ROOT))
+                setSpan(ForegroundColorSpan(context.mColor(R.color.white)), 0,
+                    this.length, Spannable.SPAN_INCLUSIVE_INCLUSIVE)
+
+                val stPoint = this.length
+                append("/{${item.quoteAsset}}")
+                setSpan(ForegroundColorSpan(context.mColor(R.color.sec_text_color)), stPoint,
+                    this.length, Spannable.SPAN_INCLUSIVE_INCLUSIVE)
+            }
         }
     }
 
--- app/src/main/java/lazycoder21/droid/crypto/utils/Utils.kt
+++ app/src/main/java/lazycoder21/droid/crypto/utils/Utils.kt
@@ -1,9 +1,37 @@
 package lazycoder21.droid.crypto.utils
 
+import android.content.Context
+import androidx.annotation.ColorRes
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.lang.reflect.Type
+
 
 object Utils {
     val Fragment.mTag: String? get() = javaClass.canonicalName
 
     fun <T> fastLazy(block: () -> T) = lazy(LazyThreadSafetyMode.NONE, block)
+
+    fun Context.mColor(@ColorRes id: Int) = ContextCompat.getColor(this, id)
+
+    fun CoroutineScope.launchIO(block: suspend CoroutineScope.() -> Unit) =
+        launch(Dispatchers.IO, block = block)
+
+    inline fun <reified T> String.parse(): T? {
+        return Moshi.Builder().build().adapter(T::class.java).fromJson(this)
+    }
+
+    inline fun <reified T> String.parseList(): List<T>? {
+        val moshi = Moshi.Builder().build()
+        val listOfCardsType: Type =
+            Types.newParameterizedType(List::class.java, T::class.java)
+        val jsonAdapter: JsonAdapter<List<T>> = moshi.adapter(listOfCardsType)
+        return jsonAdapter.fromJson(this)
+    }
 }
