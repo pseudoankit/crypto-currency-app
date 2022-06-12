d1883813ebab27966223cf6ae883e0fbbf75024b
Ankit <ankit.kumar1@tokopedia.com>
2022-06-12 12:35:31 +0530

    auto sync per 1minute

--- app/src/main/java/lazycoder21/droid/crypto/presentation/main/MainViewModel.kt
+++ app/src/main/java/lazycoder21/droid/crypto/presentation/main/MainViewModel.kt
@@ -3,6 +3,7 @@ package lazycoder21.droid.crypto.presentation.main
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
 import lazycoder21.droid.crypto.domain.repository.CryptoRepository
 import lazycoder21.droid.crypto.utils.Utils.launchIO
 import javax.inject.Inject
@@ -12,9 +13,16 @@ class MainViewModel @Inject constructor(
     private val cryptoRepository: CryptoRepository,
 ) : ViewModel() {
 
+    companion object {
+        private const val SYNC_DURATION = 1000L
+    }
+
     fun sync() {
         viewModelScope.launchIO {
-            cryptoRepository.syncListing()
+            while (true) {
+                delay(SYNC_DURATION)
+                cryptoRepository.syncListing()
+            }
         }
     }
 }
