6617bf73bf2a1dd27dc4ed64d5b74fdf5802fe93
Ankit <ankit.kumar1@tokopedia.com>
2022-06-17 07:09:18 +0530

    filter view added to display sort options

--- app/src/main/java/lazycoder21/droid/crypto/presentation/view/FilterView.kt
+++ app/src/main/java/lazycoder21/droid/crypto/presentation/view/FilterView.kt
@@ -5,37 +5,68 @@ import android.util.AttributeSet
 import android.widget.LinearLayout
 import android.widget.TextView
 import lazycoder21.droid.crypto.R
-import lazycoder21.droid.crypto.utils.Utils.fastLazy
+import lazycoder21.droid.crypto.utils.SortOptions
+import lazycoder21.droid.crypto.utils.Utils.mColor
+import lazycoder21.droid.crypto.utils.Utils.mDimension
 import lostankit7.droid.customview.FontAwesomeIcon
+import lostankit7.droid.customview.FontAwesomeIconType
+import lostankit7.droid.utils.updateTypeface
 
 class FilterView @JvmOverloads constructor(
     context: Context, private val attrs: AttributeSet? = null, defStyleAttr: Int = 0,
 ) : LinearLayout(context, attrs, defStyleAttr) {
 
+    @SortOptions
+    var sortOption: Int = SortOptions.ASCENDING
+        private set
+    var iconCTA: ((Int) -> Unit)? = null
+
     private lateinit var text: String
     private lateinit var ascIcon: String
     private lateinit var descIcon: String
-    private val textView by fastLazy { createTextView() }
-    private val icon by fastLazy { createSortIcon() }
+    private var icon: FontAwesomeIcon? = null
 
     init {
         manageAttributes()
-
+        initView()
 
     }
 
-    private fun createTextView() = TextView(context).apply {
-        text = this@FilterView.text
-        setOnClickListener { filterCTA() }
-    }
+    private fun initView() {
+        val textView = TextView(context).apply {
+            text = this@FilterView.text
+            setTextColor(context.mColor(R.color.white))
+        }
+        icon = FontAwesomeIcon(context).apply {
+            text = ascIcon
+            updateTypeface(FontAwesomeIconType.SOLID)
+            setPadding(
+                context.mDimension(R.dimen.dp_5).toInt(),
+                paddingTop, paddingRight, paddingBottom
+            )
+            setTextColor(context.mColor(R.color.white))
+        }
 
-    private fun createSortIcon() = FontAwesomeIcon(context).apply {
-        text = ascIcon
+        orientation = LinearLayout.HORIZONTAL
+        addView(textView)
+        addView(icon)
+
         setOnClickListener { filterCTA() }
     }
 
     private fun filterCTA() {
+        invertSortOptionAndUpdateIcon()
+        iconCTA?.invoke(sortOption)
+    }
 
+    private fun invertSortOptionAndUpdateIcon() {
+        sortOption = if (sortOption == SortOptions.ASCENDING) {
+            icon?.text = descIcon
+            SortOptions.DESCENDING
+        } else {
+            icon?.text = ascIcon
+            SortOptions.ASCENDING
+        }
     }
 
     private fun manageAttributes() {
@@ -50,7 +81,7 @@ class FilterView @JvmOverloads constructor(
     }
 
     companion object {
-        const val DEF_ASC_ICON = "\uf176"
-        const val DEF_DESC_ICON = "\uf175"
+        const val DEF_ASC_ICON = "\uf062"
+        const val DEF_DESC_ICON = "\uf063"
     }
 }
--- app/src/main/java/lazycoder21/droid/crypto/utils/SortOptions.kt
+++ app/src/main/java/lazycoder21/droid/crypto/utils/SortOptions.kt
@@ -2,15 +2,19 @@ package lazycoder21.droid.crypto.utils
 
 import androidx.annotation.IntDef
 import lazycoder21.droid.crypto.utils.SortOptions.Companion.ALPHABETIC
+import lazycoder21.droid.crypto.utils.SortOptions.Companion.ASCENDING
+import lazycoder21.droid.crypto.utils.SortOptions.Companion.DESCENDING
 import lazycoder21.droid.crypto.utils.SortOptions.Companion.PRICE_CHANGE
 import lazycoder21.droid.crypto.utils.SortOptions.Companion.VOLUME
 
-@IntDef(ALPHABETIC, VOLUME, PRICE_CHANGE)
+@IntDef(ALPHABETIC, VOLUME, PRICE_CHANGE, ASCENDING, DESCENDING)
 @Retention(AnnotationRetention.SOURCE)
 annotation class SortOptions {
     companion object {
-        const val ALPHABETIC = 0
-        const val VOLUME = 1
+        const val ALPHABETIC = 3
+        const val VOLUME = 4
         const val PRICE_CHANGE = 2
+        const val ASCENDING = 0
+        const val DESCENDING = 1
     }
 }
